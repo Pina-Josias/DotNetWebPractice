0991f4abe9ef541764443f42936cf25062399a5e
Josias <pina.jisrael@gmail.com>
2023-08-12 18:09:55 -0500

    added mvc project

---
+++ Northwind.Web/Pages/Customers.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using Packt.Shared;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Northwind.Web.Pages
+{
+	public class CustomersModel : PageModel
+    {
+        private readonly NorthwindContext _db;
+        public ILookup<string?, Customer>? customers;
+        public Customer? SelectedCustomer { get; private set; }
+
+        public CustomersModel(NorthwindContext db)
+        {
+            _db = db;
+        }
+
+        public void OnGet()
+        {
+            ViewData["Title"] = "Northwind B2B - Customers";
+
+            customers = _db.Customers
+                           .ToLookup(c => c.Country);
+        }
+
+        [HttpPost]
+        public IActionResult ViewFullData(Customer customer)
+        {
+            Console.WriteLine("GO TO FULL DATA");
+            SelectedCustomer = customer;
+            return RedirectToPage("customerData");
+        }
+    }
+}
--- Northwind.Web/Startup.cs
+++ Northwind.Web/Startup.cs
@@ -1,3 +1,4 @@
+using static System.Console;
 using Packt.Shared; // AddNorthWindContext extension method
 namespace Northwind.Web;
 
@@ -18,9 +19,30 @@ public class Startup
 
         app.UseRouting(); // start endpoint routing
 
+        app.Use(
+            async (HttpContext context, Func<Task> next) =>
+            {
+                RouteEndpoint? rep = context.GetEndpoint() as RouteEndpoint;
+
+                if(rep is not null)
+                {
+                    WriteLine($"Endpoint name: {rep.DisplayName}");
+                    WriteLine($"Endpoint route pattern: {rep.RoutePattern.RawText}");
+                }
+
+                if(context.Request.Path == "/bonjour")
+                {
+                    await context.Response.WriteAsync("Bonjour Monde!");
+                    return;
+                }
+
+                await next();
+            }
+        );
+
         app.UseHttpsRedirection();
 
-        app.UseDefaultFiles();
+        app.UseDefaultFiles(); // index.html, default.html, and so on
 
         app.UseStaticFiles();
 
